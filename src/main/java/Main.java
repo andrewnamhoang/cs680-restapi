095b20e7609598ff1c4678acb1d8aa6a79f6dcf5
Andrew Hoang <andrewnamhoang@gmail.com>
2016-03-08 17:05:29 -0800

    First real commit
    first real commit of files

---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+
+import static spark.Spark.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+        get("/", (req, res) -> "This is a change");
+        
+        
+    	get("/bookURL/:variable", (req, res) ->{
+    		DBDemo app = new DBDemo();
+    		String request = null;
+    		String myString = app.getABookPathAPI(req.params(":variable"));
+    		return myString;
+    	});
+
+    	get("/availableBooks",(req,res)->{
+    		DBDemo app = new DBDemo();
+    		String myString = app.getListOfBooksAPI();
+    		return myString;
+    	});
+    	
+    	}
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+}
+
